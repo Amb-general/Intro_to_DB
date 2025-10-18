6c4ef75bd869fda5ab6826ec9d762ea443d8616e
Amb-general <ambassadorgentle@gmail.com>
2025-10-18 07:06:50 +0100

    nicely done well

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,4 @@
 INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
-(2, 'Listsize_walk', 'mail@separation.com', '123 Negotiation Ave.'),
+(2, 'Blessing Malik', 'mail@sendtech.com', '224 Happiness Ave.'),
 (3, 'GeeB Broeah', 'gbroeah@jazeftech.com', '123 Negotiation Ave.'),
 (4, 'Nehealth_kamalu', 'nkamalu@jazeftech.com', '123 Negotiation Ave.');
