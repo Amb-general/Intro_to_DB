f220ed8d9cba98b0bd2b649f30e19c9772ec79e4
Amb-general <ambassadorgentle@gmail.com>
2025-10-18 07:09:57 +0100

    nicely done well right

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,4 @@
-INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'mail@sendtech.com', '224 Happiness Ave.'),
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmallk@sandtech.com', '12+ Happiness Ave.'),
 (3, 'GeeB Broeah', 'gbroeah@jazeftech.com', '123 Negotiation Ave.'),
 (4, 'Nehealth_kamalu', 'nkamalu@jazeftech.com', '123 Negotiation Ave.');
